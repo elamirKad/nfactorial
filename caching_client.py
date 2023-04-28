7c6b9c403ab1c946cb2448598f7374c41d2bccab
elamirkad <elamirkad@gmail.com>
2023-04-29 02:17:39 +0600

    feat(cache): cache client

---
+++ caching_client.py
@@ -0,0 +1,48 @@
+from threading import Thread
+import socket
+
+import select
+
+
+class CacheClient(Thread):
+    def __init__(self, port=6379):
+        Thread.__init__(self)
+        self.clientsocket = socket.socket()
+        self.clientsocket.connect((socket.gethostname(), port))
+
+    def run(self):
+        while True:
+            ready = select.select([self.clientsocket], [], [], 0.1)
+            if ready[0]:
+                data = self.clientsocket.recv(1024).decode()
+                print(data)
+        self.clientsocket.close()
+
+    def get(self, key):
+        message = f"GET\r\n{key}\r\n\r\n"
+        self.clientsocket.send(message.encode().decode('unicode_escape').encode("raw_unicode_escape"))
+        data = self.clientsocket.recv(1024).decode()
+        if data == "Done":
+            return None
+        return data
+
+    def set(self, key, value, ttl=0):
+        message = f"SET\r\n{key}\r\n{value}\r\n{ttl}"
+        self.clientsocket.send(message.encode().decode('unicode_escape').encode("raw_unicode_escape"))
+        data = self.clientsocket.recv(1024).decode()
+        return data
+
+    def delete(self, key):
+        message = f"DEL\r\n{key}\r\n\r\n"
+        self.clientsocket.send(message.encode().decode('unicode_escape').encode("raw_unicode_escape"))
+        data = self.clientsocket.recv(1024).decode()
+        return data
+
+
+cache_client = CacheClient()
+cache_client.start()
+
+cache_client.set("lol", 0)
+cache_client.delete("lol")
+result = cache_client.get("lol")
+print(result)
