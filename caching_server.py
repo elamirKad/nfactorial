216bea46e82f1e554365bbc70247ba482a865290
elamirkad <elamirkad@gmail.com>
2023-04-28 20:24:53 +0600

    feat(conn): threaded connectivity

--- caching_server.py
+++ caching_server.py
@@ -1,18 +1,29 @@
 import socket
+from threading import Thread
+
+
+def new_connection(clientsocket, address):
+    while True:
+        data = clientsocket.recv(1024).decode()
+        if not data:
+            break
+        print(str(data))
+        clientsocket.send("success!".encode())
+
+    clientsocket.close()
+
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 serversocket.bind((socket.gethostname(), 6379))
-
-serversocket.listen(2)
+serversocket.listen(5)
 print("Waiting for connections...")
-(clientsocket, address) = serversocket.accept()
-print("Received connection from", clientsocket)
-while True:
-    data = clientsocket.recv(1024).decode()
-    if not data:
-        break
-    print(str(data))
-    clientsocket.send("success!".encode())
 
-clientsocket.close()
+while True:
+    (clientsocket, address) = serversocket.accept()
+    print("Received connection from", clientsocket)
+    conn = Thread(target=new_connection, args=(clientsocket, address))
+
+    conn.start()
+
+
+serversocket.close()
